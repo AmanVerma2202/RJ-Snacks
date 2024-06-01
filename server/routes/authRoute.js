6d0f132ab22e2b11057a2e5d0ee6faead6ccccb1
Aman Verma <aka.amanverma@gmail.com>
2024-06-01 12:25:01 +0530

    code pushed
    rj&snacks

---
+++ server/routes/authRoute.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import {registerController,loginController, testController, forgotPasswordController, updateProfileController, getOrdersController} from '../controllers/authController.js'
+
+// router object
+
+const router = express.Router()
+
+
+//routing 
+//REGISTER || METHOD POST 
+
+router.post('/register',registerController)
+
+//LOGIN || POST
+router.post('/login',loginController)
+
+// Forget password || POST
+router.post('/forgot-password',forgotPasswordController)
+
+//test routes 
+router.get("/test",requireSignIn,isAdmin , testController)
+
+
+//protected user route 
+router.get('/user-auth',requireSignIn,(req,res)=>{
+    res.status(200).send({ok:true});
+})
+
+// protected admin route
+router.get('/admin-auth',requireSignIn,isAdmin,(req,res)=>{
+    res.status(200).send({ok:true});
+})
+
+//update profile
+router.put('/profile',requireSignIn,updateProfileController)
+
+//orders 
+router.get('/orders',requireSignIn,getOrdersController)
+
+export default router
+
